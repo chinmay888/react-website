2310857a96544546c1b2651f7a6ce4061253d41c
Chinmay Bhat <0310chinmay@gmail.com>
2023-03-12 02:47:00 +0530

    test

---
+++ pages/footer.jsx
@@ -0,0 +1,22 @@
+
+import './footer.css';
+
+function footer() {
+  return (
+    <div id='footer-div'>
+      <div className='aboutus-div'>
+        <p className='aboutus-text1'>
+          About Us
+        </p>
+        <p className='aboutus-text2'>
+        Ovo Farm Fresh is one of the most<br></br>
+        advanced layer farm with fully<br></br>
+        automated state-of-art technology<br></br>
+        in India.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default footer;
